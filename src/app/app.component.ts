3a6bad438c639d027178057316d8d6f000c3599b
Stéphane Nsakala <stephane.nsakala@luxairgroup.lu>
2018-02-27 11:22:42 +0100

    tslint fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 import { ITEMSET, SEARCHRESULTS } from './mock-set';
 import { Item, Change, Action } from './item';
 
@@ -9,13 +9,13 @@ import { Item, Change, Action } from './item';
 })
 export class AppComponent {
   itemSet = ITEMSET;
-  searchResults = SEARCHRESULTS; 
+  searchResults = SEARCHRESULTS;
   changes: Change[] = [];
 
   remove(item: Item): void {
-    let change = {action: Action.Remove, item: item};
-    let invChange = {action: Action.Add, item: item};
-    let invChangeIndex = this.changes.findIndex(
+    const change = {action: Action.Remove, item: item};
+    const invChange = {action: Action.Add, item: item};
+    const invChangeIndex = this.changes.findIndex(
       (a) => a.action === invChange.action && a.item === invChange.item
     );
     console.log(change)
@@ -25,33 +25,33 @@ export class AppComponent {
       return;
     }
     this.changes.push(change);
-    let index = this.itemSet.items.indexOf(item);
-    console.log(item)
+    const index = this.itemSet.items.indexOf(item);
+    console.log(item);
     this.itemSet.items.splice(index, 1);
     this.searchResults.push(item);
     console.log('remove', index, item);
   }
 
   add(item: Item): void {
-    let change = {action: Action.Add, item: item};
-    let invChange = {action: Action.Remove, item: item};
-    let invChangeIndex = this.changes.findIndex(
+    const change = {action: Action.Add, item: item};
+    const invChange = {action: Action.Remove, item: item};
+    const invChangeIndex = this.changes.findIndex(
       (a) => a.action === invChange.action && a.item === invChange.item
     );
-    
+
     console.log(change)
     console.log(invChangeIndex);
-    if (invChangeIndex != -1) {
+    if (invChangeIndex !== -1) {
       this.changes.splice(invChangeIndex, 1);
     } else {
       this.changes.push(change);
     }
-    
+
     this.itemSet.items.push(item);
     this.itemSet.items = this.itemSet.items.sort(
       (a,b) => a.displayName > b.displayName ? 1:0
     );
-    let index = this.searchResults.indexOf(item);
+    const index = this.searchResults.indexOf(item);
     this.searchResults.splice(index, 1);
     console.log('add', index, item);
   }
